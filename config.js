10b0ce8c76c2e4e20e6268a1610287c06fe8dcac
lugat1000 <lugat1000@gmail.com>
2026-03-17 09:48:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // Wird von index.html und widget.html genutzt
 // ═══════════════════════════════════════════
 const CONFIG = {
-  API_URL: 'DEINE_APPS_SCRIPT_WEB_APP_URL',
-  USER_ID: '',  // z.B. 'lukas@chmedia.ch' oder leer lassen
+  API_URL: 'https://script.google.com/macros/s/AKfycbyvZSTsnV8Nb5rQc3nmOZcFa4HACWxz6LFJiw3jHk7Mh6N-VsVE_yvRBR0JqherH6NnPw/exec',
+  USER_ID: 'lukas.dreier@chmedia.ch',  // z.B. 'lukas@chmedia.ch' oder leer lassen
 };
